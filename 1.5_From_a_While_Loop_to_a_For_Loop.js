998c2ae0019b432bd4e4d0c8e8514a85d7277cb3
Zeous06 <edwinhernandezaponte@yahoo.com>
2017-05-15 19:47:05 -0400

    first commit

---
+++ 1.5_From_a_While_Loop_to_a_For_Loop.js
@@ -0,0 +1,15 @@
+// Video 1.4 accompanies this exercise
+
+// Write a basic for loop that prints to the console all
+// the numbers from 10 to 1 in descending order. This is the similar to one
+// of the previous challenges, but this time we’re using a for loop instead
+// of a while loop.
+for(let number = 10; number >= 1; number--) {
+  console.log(number)
+}
+// Wrong loop - the first and second condition requires that a loop continues
+to run because 10 is greater than 1 and  the third contidion requires
+to keep to the count for (let number = 10; number >= 1; number++)
+within the third condition to for(let number = 10; number >= 1; number--)
+the loop stops when the condition is false
+therefore 0 is not greater than 1. loops stops.
